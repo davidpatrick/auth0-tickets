a7e0e7091b7c4c7b4bb1ce55957cbf65a9c0d50f
David Patrick <dpatrick@lightspeedsystems.com>
2016-07-25 13:50:59 -0700

    Add Header to Page

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,25 @@ export default class App extends React.Component {
 
     return (
       <div>
-        {children}
+        <header className="site-header">
+          <nav role="navigation" className="navbar navbar-default">
+            <div className="container">
+              <div className="navbar-header">
+                <h1 className="navbar-brand"><a href="/"><span>Auth0</span></a></h1>
+              </div>
+              <div id="navbar-collapse" className="collapse navbar-collapse">
+                <ul className="nav navbar-nav navbar-left no-basic">
+                  <li><a href="/#/home">Home</a></li>
+                  <li><a href="/#/logout">Logout</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <div className="container">
+          {children}
+        </div>
       </div>
     );
   }
