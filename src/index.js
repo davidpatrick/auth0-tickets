3f5c22a18ce7ca22c1dec9ab7e80c0b1f7c97e00
David Patrick <dpatrick@lightspeedsystems.com>
2016-07-26 00:33:09 -0700

    Inject Redux into AuthService

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ import NotFound from './components/NotFound';
 import Home from './components/Home';
 import Login from './components/Login';
 
-const auth = new AuthService(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN);
 const store = createStore(reducers, applyMiddleware(ReduxThunk));
+const auth = new AuthService(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_DOMAIN, store);
 
-// onEnter callback to validate authentication in private routes
 const requireAuth = (nextState, replace) => {
   if (!auth.loggedIn()) {
     replace({ pathname: '/login' });
@@ -26,7 +25,7 @@ render(
     <Router history={hashHistory}>
       <Route path="/" component={App} auth={auth}>
         <IndexRedirect to="/home" />
-        <Route path="home" component={Home}  onEnter={requireAuth}/>
+        <Route path="home" component={Home} onEnter={requireAuth}/>
         <Route path="login" component={Login} />
         <Route path="*" component={NotFound}/>
       </Route>
--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -1,18 +1,33 @@
 import Auth0Lock from 'auth0-lock';
+import { loginUser, logoutUser } from '../actions/';
 
 export default class AuthService {
-  constructor(clientId, domain) {
+  constructor(clientId, domain, store) {
     // Configure Auth0
     this.lock = new Auth0Lock(clientId, domain, {});
     // Add callback for lock `authenticated` event
     this.lock.on('authenticated', this._doAuthentication.bind(this));
     // binds login functions to keep this context
     this.login = this.login.bind(this);
+    // binds redux store
+    this.reduxStore = store;
+    // binds redux dispatch
+    this.dispatch = store.dispatch;
   }
 
   _doAuthentication(authResult){
     // Saves the user token
     this.setToken(authResult.idToken);
+    this.dispatchLogin(authResult.idToken);
+  }
+
+  dispatchLogin(token) {
+    // Dispatches the user token to the redux store
+    this.dispatch(loginUser(token));
+  }
+
+  dispatchLogout() {
+   this.dispatch(logoutUser()); 
   }
 
   login() {
@@ -22,7 +37,14 @@ export default class AuthService {
 
   loggedIn(){
     // Checks if there is a saved token and it's still valid
-    return !!this.getToken();
+    const token = this.getToken();
+
+    // Dispatch Login and Token if not already set
+    if (token && !this.reduxStore.getState().authentication.loggedIn) {
+      this.dispatchLogin(token);
+    }
+
+    return !!token;
   }
 
   setToken(idToken){
@@ -38,5 +60,6 @@ export default class AuthService {
   logout(){
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
+    this.dispatchLogout();
   }
 }
