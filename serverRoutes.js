476bba6c3caa67d4cab414e34673d7bd380b60d7
David Patrick <dpatrick@lightspeedsystems.com>
2016-07-26 23:58:43 -0700

    Server Authentication
    - Validating the users Auth0 token and setting the user as a currentUser on the request.
    - Started Ticket Submission Route and added some lightweight form validation.

--- serverRoutes.js
+++ serverRoutes.js
@@ -1,15 +1,12 @@
+import fetch from 'isomorphic-fetch';
 import path from 'path';
 import { Router } from 'express';
 
 const serverRoutes = Router();
 
+// Front-End Routes
 serverRoutes.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
-});
-
-serverRoutes.post('/api/v1/submit_ticket', (req, res) => {
-  console.log('body', req.body);
-  res.json(req.body);
 });
 
 serverRoutes.get('*', function(req, res) {
@@ -17,5 +14,56 @@ serverRoutes.get('*', function(req, res) {
     'route': 'Sorry this page does not exist!'
   });
 });
+  
+// Api Routes
+serverRoutes.all('/api/*', requireAuthentication);
+
+serverRoutes.post('/api/v1/submit_ticket', (req, res) => {
+  const error = validateForm(req.body);
+
+  if (error) {
+    res.status(400).json({ error: error });
+  } else {
+    submitToZendesk(req.body);
+    res.json(req.body);
+  }
+
+  function submitToZendesk(form) {
+    console.log('submitToZendesk', form);
+  }
+
+  function validateForm(form) {
+    if (!hasRequiredFields(form)) {
+      return 'Missing Required Fields';
+    } else {
+      return null;
+    }
+  }
+
+  function hasRequiredFields(form) {
+    return !!form.name && !!form.email && !!form.subject && !!form.body;
+  }
+});
+
+function requireAuthentication (req, res, next) {
+  const authToken = req.header('Auth-Token');
+
+  fetch('https://foray.auth0.com/tokeninfo', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      id_token: authToken
+    })
+  }).then(response => {
+    if (response.status === 200) {
+      response.json().then(json => {
+        req.currentUser = json;
+        next();
+      });
+    } else {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+  }).catch(error=> res.status(500).json({ error: error }));
+}
 
 export default serverRoutes;
