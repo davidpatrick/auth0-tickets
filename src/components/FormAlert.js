942a91f27b9a4296b0b2dc23a00138bd53f787f4
David Patrick <dpatrick@lightspeedsystems.com>
2016-07-28 18:34:25 -0700

    Update Form Alert

--- src/components/FormAlert.js
+++ src/components/FormAlert.js
@@ -3,13 +3,17 @@ import FormInput from './FormInput';
 
 const FormAlert = ({success, error}) => {
   if (success) {
-    return <div className="alert alert-success">
-      This form does not yet post real data!  This is a test.
-    </div>;
+    return (
+      <div className="alert alert-success">
+        Ticket successfully created!
+      </div>
+    );
   } else if (error) {
-    return <div className="alert alert-danger">
-      There was an error: {error}
-    </div>;
+    return (
+      <div className="alert alert-danger">
+        There was an error: {error}
+      </div>
+    );
   } else {
     return  null;
   }
